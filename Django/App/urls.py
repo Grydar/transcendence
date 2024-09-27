f8d3cde0adb89d1c75457ccb8add91af77a1549e
rstride <romain.stride@gmail.com>
2024-09-27 11:32:25 +0200

    [UPD] Initialize Django project structure and add Docker support

--- Django/App/urls.py
+++ Django/App/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for ft_transcendence project.
+URL configuration for App project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,11 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-	path('', TemplateView.as_view(template_name='index.html'), name='home'),
-    path('api/', include('pong.urls')),
 ]
